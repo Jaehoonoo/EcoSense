ee182b6917e2f6ca084b9aba8de934831fadc8ba
AmielHalili <str8lanes@gmail.com>
2024-10-06 09:14:57 -0400

    bills start

--- app/dashboard/hub/page.js
+++ app/dashboard/hub/page.js
@@ -1,5 +1,10 @@
+import Bills from './Bills'; // Adjust the path as needed
+
 export default function Hub() {
     return (
-        <div>hub</div>
-    )
+        <div>
+            <h1>Energy Bill Calculator</h1>
+            <Bills />
+        </div>
+    );
 }
